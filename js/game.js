7984177c3b48c9851d2d2dd24cde034a23fd19e3
casesensative <victor.waters1@gmail.com>
2021-04-01 13:32:28 -0400

    Issue with inBounds and generateTiles

--- js/game.js
+++ js/game.js
@@ -6,4 +6,32 @@ function setupCanvas() {
   canvas.height = tileSize * numTiles;
   canvas.style.width = canvas.width + 'px';
   canvas.style.height = canvas.height + 'px';
+  ctx.imageSmoothingEnabled = false;
 }
+
+function drawSprite(sprite, x, y) {
+  ctx.drawImage(
+    spritesheet,
+    sprite*16,
+    0,
+    16,
+    16,
+    x*tileSize,
+    y*tileSize,
+    tileSize,
+    tileSize
+  );
+}
+
+
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  for (let i = 0; i < numTiles; i++) {
+    for (let j = 0; j < numTiles; j++) {
+      getTile(i, j).draw();
+    }
+  }
+  drawSprite(0, x, y);
+};
